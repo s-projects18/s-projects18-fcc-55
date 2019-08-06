912d4ed48d03f37f266abd075d801ee7dc7983ad
Glitch (s-projects18-fcc-55) <none>
2019-08-06 12:19:04 +0000

    delete-files, index.html

--- public/fe.js
+++ public/fe.js
@@ -7,6 +7,18 @@ $("document").ready(function(){
       });
       $('#myiframe').contents().find('body').html(r);
     });  
-  });  
+  });
+  
+  $('#deleteFiles').click(function(){
+    if(window.confirm('Delete all files?')) {
+      $.getJSON('/api/del-files', function(obj){
+        var r = '';
+        $.each(obj.deletedFiles, function(k,v){
+          r += v + "<br>";
+        });
+        $('#myiframe').contents().find('body').html(r);
+      }); 
+    }
+  });
 });
 
